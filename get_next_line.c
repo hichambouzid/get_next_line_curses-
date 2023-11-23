fd17696900886471cf53d988f83bac1979e83a5d
hichambouzid <bouzidhicham51@gmail.com>
2023-11-23 12:54:53 +0100

    fix some errors

--- get_next_line.c
+++ get_next_line.c
@@ -5,141 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/20 11:04:28 by hibouzid          #+#    #+#             */
-/*   Updated: 2023/11/22 11:50:02 by hibouzid         ###   ########.fr       */
+/*   Created: 2023/11/22 14:51:10 by hibouzid          #+#    #+#             */
+/*   Updated: 2023/11/23 11:58:06 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
 int ft_check(char *str, char c)
 {
-    int i;
+	int i;
 
-    i = 0;
-	if (!str)
-		return (-1);
-    while (str[i])
-    {
-        if (str[i] == c)
-            return (i);
-        i++;
-    }
-	if (str[i] == 0 && i)
-		return (i);
-    return (-1);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
-
 
 char *get_read(char *ptr, int fd)
 {
-    char *str;
-    ssize_t i;
-
-    i = 1;
-	if (BUFFER_SIZE)
-    	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!str)
-        return (NULL);
-    while (ft_check(str, '\n') == -1)
-    {
-        i = read(fd, str, BUFFER_SIZE);
-        if (i == -1)
-        {
-            free(str);
-			free(ptr);
-            return (NULL);
-        }
-        ptr = ft_strjoin(ptr, str);
-		if (i == 0)
+	int i;
+	char *str;
+	
+	i = 0;
+	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!str)
+		return (NULL);
+	str[0] = 0;
+	while (ft_check(str, '\n') == -1)
+	{
+		i = read(fd, str, BUFFER_SIZE);
+		if (i < 0 || i == 0)
 		{
+			free(ptr);
 			free(str);
-			return (ptr);
+			return (NULL);
 		}
-    }
-	free(str);
+		str[i] = 0;
+		ptr = ft_strjoin(ptr, str);
+		if (ft_strlen(str) > BUFFER_SIZE)
+			break;
+	}
+	free (str);
 	return (ptr);
 }
-
-char	*ft_substr(char *str, int start, int len)
+char *ft_strlcpy(char *dst, char *src, int len)
 {
 	int i;
-	char *ptr;
 
-	ptr = malloc(sizeof(char) * (1 + len - start));
-	if (!str)
-		return (NULL);
 	i = 0;
-	while (i <= len)
+	while (*src && i < len)
 	{
-		ptr[i] = str[i];
+		dst[i] = *src;
+		src++;
 		i++;
 	}
-	ptr[i] = 0;
-	return (ptr);
+	dst[i] = '\n';
+	dst[i + 1] = 0;
+	return (dst);
 }
-
-char *ft_cutstring(char *ptr, int i)
+char *ft_line(char *ptr, int f)
 {
-	int tmp;
+	int i;
 	char *str;
-
-	if (ptr[i + 1])
-		i++;
-	tmp = i;
-	while (ptr[i])
-		i++;
-	str = malloc(sizeof(char) * (2 + i - tmp));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (ptr[tmp])
-	{
-		str[i] = ptr[tmp];
-		i++;
-		tmp++;
-	}
-	str[i] = 0;
-	return (str);
-}
-
-char *get_next_line(int fd)
-{
-    static char	*ptr;
-	char		*str;
-	char		*tmp;
-	int			i;
-
-    if (fd == -1)
-        return (NULL);
-    ptr = get_read(ptr, fd);
-	if (!ptr)
-		return (NULL);
-	if (*ptr == '\n')
-	{
-		i = ft_check(ptr + 1, '\n');
-		str = ft_substr(ptr + 1, 0, i);
-	}
-	else
-	{	
-		i = ft_check(ptr, '\n');
-		str = ft_substr(ptr, 0, i);
-	}
-	tmp = ft_cutstring(ptr, i);
-	free(ptr);
-	ptr = tmp;
-	return (str);
-}
-
-// int main()
-// {
-//     int fd = open("test", O_RDONLY);
-// 	if (fd < 0)
-// 		return (0);
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
 	
-// }
+	// printf("all ptr is :%s\n", ptr);
+	i = ft_check(ptr + f, '\n');
+	// printf("%d\n", i);
+	str = malloc(sizeof(char) * (2 + i));
+	if (!str)
+	return (NULL);
+	str
+	
+}
+
+char	*get_next_line(int fd)
+{
+	static char *ptr;
+	static int f;
+	
+	if (fd < 0)
+		return (NULL);
+	ptr = get_read(ptr, fd);
+	return (ft_line(ptr , f));
+}
+
+int main()
+{
+	int fd = open("test" ,O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+	// printf("%s", get_next_line(fd));
+	// printf("%s", get_next_line(fd));÷
+	//get_next_line(fd);
+	// get_next_line(fd);
+	// get_next_line(fd);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:02:26 by hibouzid          #+#    #+#             */
-/*   Updated: 2023/11/22 10:29:04 by hibouzid         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:25:38 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@
 # include <stdio.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 100
 # endif
 int		ft_check(char *str, char c);
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *str);
 int		ft_strlen(char *str);
-char	*ft_strrchr(char *s, int c);
+// char	*ft_just_line(char *str, int f);
+char	*get_read(char *ptr, int fd);
+
+// char	*ft_strrchr(char *s, int c);
 
 #endif
