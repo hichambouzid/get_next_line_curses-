29b90a8c7fb0ceebe6dac4aa745cddc96160ee3d
hichambouzid <bouzidhicham51@gmail.com>
2023-11-24 11:52:37 +0100

    fixe the norm and some leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:51:10 by hibouzid          #+#    #+#             */
-/*   Updated: 2023/11/23 21:24:28 by hibouzid         ###   ########.fr       */
+/*   Updated: 2023/11/24 11:51:14 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,72 +31,97 @@ char *get_read(char *ptr, int fd)
 	int i;
 	char *str;
 	
-	i = 0;
+	i = 1;
 	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!str)
 		return (NULL);
 	str[0] = 0;
-	while (ft_check(str, '\n') == -1)
+	while (ft_check(str, '\n') == -1 && i != 0)
 	{
 		i = read(fd, str, BUFFER_SIZE);
-		if (i < 0 || i == 0)
+		if (i < 0 )
 		{
 			free(str);
-			if (!ptr)
-				return (ptr);
-			return (ptr);
+			free(ptr);
+			return (NULL);
 		}
 		str[i] = 0;
 		ptr = ft_strjoin(ptr, str);
-		if (ft_strlen(str) < BUFFER_SIZE || !ptr)
-			break;
 	}
 	free (str);
 	return (ptr);
 }
 
-char *ft_line(char *ptr, int *f)
+char *ft_cut(char *ptr)
 {
 	int i;
 	char *str;
-	int j;
-	
-	j = 0;
-	i = *f;
-	while (ptr[i] != '\0')
-	{
+
+	i = 0;
+	if (!ptr)
+		return (NULL);
+	while (ptr[i] && ptr[i] != '\n')
 		i++;
-		if (ptr[i] == '\n')
-		break;
-	}
-	if (i == *f)
+	if (ptr[i] == '\n')
+		i++;
+	if (!ptr[i])
 	{
 		free(ptr);
 		return (NULL);
 	}
-	str = malloc(sizeof(char) * (2 + i - *f));
-	while (*f <= i)
-	{
-		str[j] = ptr[*f];
-		j++;
-		*f += 1;
-	}
-	str[j] = 0;
+	str = ft_strdup(ptr + i);
+	free(ptr);
 	return (str);
+}
+
+char *ft_free(char *str, char *ptr)
+{
+	if (*str == 0)
+	{
+		free(str);
+		free(ptr);
+		return (NULL);
+	}
+	return (str);
+}
+char *ft_line(char *ptr)
+{
+	int i;
+	char *str;
+
+	i = 0;
+	if (!ptr)
+		return (NULL);
+	while (ptr[i] && ptr[i] != '\n')
+	i++;
+	if (i == 0 && !ptr[0])
+		return (NULL);
+	str = malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (ptr[i] && ptr[i] != '\n')
+	{
+		str[i] = ptr[i];
+		i++;
+	}
+	if (ptr[i] != 0)
+		str[i++] = '\n';
+	str[i] = 0;
+	return (ft_free(str, ptr));
 }
 
 char	*get_next_line(int fd)
 {
 	static char *ptr;
-	static int f;
 	char *str;
 
 	str = NULL;
-	if (fd == -1)
+	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (NULL);
 	ptr = get_read(ptr, fd);
-	if (ptr)
-		str = ft_line(ptr, &f);
+	str = ft_line(ptr);
+	ptr = ft_cut(ptr);
 	return (str);
 }
 
@@ -113,12 +138,12 @@ char	*get_next_line(int fd)
 // 	// get_next_line(fd);
 // 	// get_next_line(fd);
 // }
-// int main(int ac, char **av){
-// 	int fd = open(av[1] ,O_RDONLY);
-// 	char *line = get_next_line(fd);
-// 	while(line){
-// 		printf("%s",line);
-// 		line = get_next_line(fd);
-// 	}
-// 	return 0;
-// }
+int main(int ac, char **av){
+	int fd = open(av[1] ,O_RDONLY);
+	char *line = get_next_line(fd);
+	while(line){
+		printf("%s",line);
+		line = get_next_line(fd);
+	}
+	return 0;
+}
